8ae8cabd3320abe90eabd2a43b02aa8172539607
todojinu <nukukkoya@gmail.com>
2022-09-25 16:54:16 +0900

    3.useEffect 의 cleanup function 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  const byFn = () => {
+    console.log("bye :(");
+  };
+  const hiFn = () => {
+    console.log("created :)");
+    return byFn;
+  };
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []); // 최초 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); // 최초 실행 or keyword 변경될 때 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]); // 최초 실행 or counter 변경될 때 실행
-  useEffect(() => {
-    console.log("I run when keyword & counter change.");
-  }, [keyword, counter]); // 최초 실행 or keyword or counter 변경될 때 실행
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
+
 export default App;
